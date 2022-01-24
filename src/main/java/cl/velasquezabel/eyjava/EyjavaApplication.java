4c60b7e3f40b3d97b605a22cd984462e5771c469
Abel Velasquez <velasquez.abel@gmail.com>
2022-01-24 15:29:05 -0300

    initial project layout , with gradle build, for main app and JUnit tests

---
+++ src/main/java/cl/velasquezabel/eyjava/EyjavaApplication.java
@@ -0,0 +1,13 @@
+package cl.velasquezabel.eyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/velasquezabel/eyjava/EyjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.velasquezabel.eyjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
