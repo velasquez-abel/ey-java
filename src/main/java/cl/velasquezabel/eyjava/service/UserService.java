a252305258e1ebdd529643ecab66ea294f9df8ab
ABEL VELASQUEZ <velasquez.abel@gmail.com>
2022-01-25 17:45:12 -0300

    adding JPA code for persistance on service layer
    adding JPA code for persistance on service layer

--- src/main/java/cl/velasquezabel/eyjava/service/UserService.java
+++ src/main/java/cl/velasquezabel/eyjava/service/UserService.java
@@ -5,23 +5,52 @@ import cl.velasquezabel.eyjava.dto.UserDTO;
 
 public class UserService{
 
+	@Autowired
+	private UserRepository repository;
+
 	protected UserService(){
 		
 	}
-	public MessageDTO insertUser( UserDTO user ){
-		return null;
+	public MessageDTO insertUser( UserDTO userDTO ){
+		User user = new User();
+		user.set( userDTO.get );
+		
+		repository.save(user);
 	}
 	
 	public UserDTO lookupUser(String id){
-		return null;
+		User user = repository.find(id);
+		UserDTO userDTO = new UserDTO();
+		userDTO.set( user.get );
+		
+		return userDTO;
+	}
+	
+	public List<UserDTO> lookupAll(){
+		List<User> list = repository.findAll();
+		List<UserDTO> listDTO = new ArrayList<UserDTO>();
+		for( User u : list ){
+			UserDTO dto = new UserDTO();
+			dto.set( u.get() );
+		}
+		return listDTO;
 	}
 	
 	public MessageDTO deleteUser(String id){
-		return null;
+		MessageDTO msg = new MessageDTO();
+		repository.remove(id);
+		msg.setMessage( "user has been deleted" );
+		return msg;
 	}
 	
 	public MessageDTO updateUser(UserDTO user){
-		return null;
+		MessageDTO msg = new MessageDTO();
+		User user = new User();
+		user.set( userDTO.get );
+		
+		repository.save(user);
+		msg.setMessage( "user has been updated" );
+		return msg;
 	}
 
 
