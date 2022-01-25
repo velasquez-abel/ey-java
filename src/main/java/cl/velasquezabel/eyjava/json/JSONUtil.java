a812ac055e6af1e1f1e364b77f43c8e2ddae3636
ABEL VELASQUEZ <velasquez.abel@gmail.com>
2022-01-24 22:59:26 -0300

    adding utility classes for integration
    added utility classes and POJOs for integration between endpoint layer
    and persistance layer

---
+++ src/main/java/cl/velasquezabel/eyjava/json/JSONUtil.java
@@ -0,0 +1,11 @@
+
+
+public final class JSONUtil{
+
+private JSONUtil(){}
+
+public final static UserJSON getUserJSON( UserDTO u ){}
+public final static MessageJSON getMessageJSON( MessageDTO m ){}
+public final static UserDTO getUserDTO( UserJSON u ){}
+public final static MessageDRO getMessageDTO( MessageDTO m){}
+}
---
+++ src/main/java/cl/velasquezabel/eyjava/service/UserService.java
@@ -0,0 +1,15 @@
+
+
+public class UserService{
+
+public MessageDTO insertUser( UserDTO user )
+
+public UserDTO lookupUser(String id){
+}
+
+public MessageDTO deleteUser(String id){}
+
+public MessageDTO updateUser(UserDTO user){}
+
+
+}
